6eabe77b0fb0b7576c708325b3586967152962c1
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-01 17:17:38 +1100

    Increase SLURM job time allocation in submit_bash.sh to accommodate longer training sessions

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=00:20:00           # Increased time for longer training with larger batches
+#SBATCH --time=03:20:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
