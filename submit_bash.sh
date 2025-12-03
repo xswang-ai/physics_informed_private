310b8470deb62ef79167f5e6c40710f08f29e181
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-03 16:20:25 +1100

    Comment out evaluation command in submit_bash.sh for Customized-Re500-HFS-1s-100.yaml to prevent accidental execution during training.

--- submit_bash.sh
+++ submit_bash.sh
@@ -40,7 +40,7 @@ python3 train_operator.py --config_path configs/pretrain/Customized-Re500-Wavele
 # python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-HFS-1s-100.yaml --test_ratio 0.25
 
 
-python3 eval_operator_2d.py --config_path configs/pretrain/Customized-Re500-HFS-1s-100.yaml
+# python3 eval_operator_2d.py --config_path configs/pretrain/Customized-Re500-HFS-1s-100.yaml
 
 # python3 eval_operator_2d.py --config_path configs/pretrain/Customized-Re500-FNO2d-1s-100.yaml
 
