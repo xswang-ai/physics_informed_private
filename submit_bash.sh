20b7ddc0a9444dcb365fd390452223017c34d97f
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-07 13:14:06 +1100

    Update submit_bash.sh to include HFS2d training command and comment out WNO2d command
    This commit adds a new command to train the HFS2d model using the specified configuration file while commenting out the existing command for the WNO2d model. This change facilitates the training process for the new model while maintaining the option to run the WNO2d training in the future.

--- submit_bash.sh
+++ submit_bash.sh
@@ -31,8 +31,11 @@ echo "=========================================="
 # Training
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-FNO2d-1s-100.yaml --test_ratio 0.25
+# python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-FNO2d-1s-100.yaml --test_ratio 0.25
 
+python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-HFS2d-1s-100.yaml --test_ratio 0.25
+
+# python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-WNO2d-1s-100.yaml --test_ratio 0.25
 
 # python3 eval_operator_2d.py --config_path configs/pretrain/Customized-Re500-WNO2d-1s-100.yaml
 
