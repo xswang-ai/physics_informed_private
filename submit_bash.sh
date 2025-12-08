e770abd7142c9ce6e6cb82037e6f5edb187c0ef1
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-08 15:13:56 +1100

    Update submit_bash.sh to reduce training time to 4 hours and replace the InnerWavelet2D training command with a new command for MultiscaleWavelet2D, reflecting a shift in training focus.

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=6:00:00           # Increased time for longer training with larger batches
+#SBATCH --time=4:00:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -37,7 +37,9 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-WNO2d-1s-100.yaml --test_ratio 0.25
 
-python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-InnerWavelet2D-1s-100.yaml --test_ratio 0.25 --resume_training --resume_ckpt InnerWavelet2D-Re500-1s-100_34001.pt
+# python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-InnerWavelet2D-1s-100.yaml --test_ratio 0.25 --resume_training --resume_ckpt InnerWavelet2D-Re500-1s-100_34001.pt
+
+python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-MultiscaleWavelet2d-1s-100.yaml --test_ratio 0.25
 
 # python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-InnerWaveletPatching2d-1s-100.yaml --test_ratio 0.25
 
