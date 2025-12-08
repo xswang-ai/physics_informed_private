f2bbc09097085fb426786a26e5cf59c2933d2890
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-08 12:12:11 +1100

    Update submit_bash.sh to reduce training time to 1 hour and comment out the SAOT2d training command, reflecting a shift in focus for the training process.

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=05:00:00           # Increased time for longer training with larger batches
+#SBATCH --time=1:00:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -31,7 +31,7 @@ echo "=========================================="
 # Training
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-# python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-FNO2d-1s-100.yaml --test_ratio 0.25
+python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-FNO2d-1s-100.yaml --test_ratio 0.25
 
 # python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-HFS2d-1s-100.yaml --test_ratio 0.25
 
@@ -41,7 +41,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-InnerWaveletPatching2d-1s-100.yaml --test_ratio 0.25
 
-python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-SAOT2d-1s-100.yaml --test_ratio 0.25
+# python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-SAOT2d-1s-100.yaml --test_ratio 0.25
 
 # python3 eval_operator_2d.py --config_path configs/pretrain/Customized-Re500-WNO2d-1s-100.yaml
 
