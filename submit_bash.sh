b0450b147cc06602116b9948e17ab865cc2ed12d
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-10 18:19:16 +1100

    Refactor submit_bash.sh to enable training of the MultiscaleWavelet model without resuming from a checkpoint. Update MultiscaleWaveletTransformer2D class to incorporate efficient attention mechanism and adjust model parameters in the YAML configuration for improved performance.

--- submit_bash.sh
+++ submit_bash.sh
@@ -41,7 +41,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-InnerWavelet2D-1s-100.yaml --test_ratio 0.25 --resume_training --resume_ckpt InnerWavelet2D-Re500-1s-100_34001.pt
 
-# python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-MultiscaleWavelet2d-1s-100.yaml --test_ratio 0.25 --resume_training --resume_ckpt MultiscaleWavelet2d-Re5001s-100_27001.pt
+python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-MultiscaleWavelet2d-1s-100.yaml --test_ratio 0.25
 
 # python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-InnerWaveletPatching2d-1s-100.yaml --test_ratio 0.25
 
