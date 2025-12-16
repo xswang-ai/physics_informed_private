a01a1207df875c425d2b23467bffe4b0fff8eea7
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-16 12:16:53 +1100

    Update submit_bash.sh to extend training time for larger batches and modify training command for MSWTStableSoft2d model to include resume options, ensuring proper execution of training workflow.

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=00:30:00           # Increased time for longer training with larger batches
+#SBATCH --time=02:30:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -47,7 +47,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-MSWTStable2d-1s-100.yaml --test_ratio 0.25
 
-# python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-MSWTStableSoft2d-1s-100.yaml --test_ratio 0.25
+python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-MSWTStableSoft2d-1s-100.yaml --test_ratio 0.25 --resume_training --resume_ckpt MSWTStableSoft2d-Re500-1s-100_17001.pt
 
 
 # python3 train_operator_2d.py --config_path configs/pretrain/Customized-Re500-InnerWaveletPatching2d-1s-100.yaml --test_ratio 0.25
@@ -71,7 +71,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # # python3 eval_operator_2d.py --config_path configs/pretrain/Customized-Re500-WNO2d-1s-100.yaml
 
-python3 eval_operator_2d.py --config_path configs/test/Customized-Re500-MSWTStable2d-05s-test.yaml
+# python3 eval_operator_2d.py --config_path configs/test/Customized-Re500-MSWTStable2d-05s-test.yaml
 
 ######################################################## TRAINING 3D ##################################################################
 
