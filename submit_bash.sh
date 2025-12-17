b16fdffeb321ef46dc92f986a66de33a94c4c3e6
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-17 19:47:06 +1100

    Update submit_bash.sh to extend training time for larger batches from 30 minutes to 3 hours and 30 minutes, accommodating longer training sessions.

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=00:30:00           # Increased time for longer training with larger batches
+#SBATCH --time=03:30:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
